2fcfbd2a0ecfe3c10ddad0415726e0d20f4c8b80
Shera97 <93433298+Shera97@users.noreply.github.com>
2021-10-31 02:04:08 +0300

    Update MrTiger.js

--- MrTiger.js
+++ MrTiger.js
@@ -22,10 +22,10 @@ client.on("ready", () => {
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
     client.user.setActivity(STREAMING, {      
-    type:"Playing", 
+    type:"RUSSI UP", 
     url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
 
-client.login("");
+client.login("mfa.TRy4tqU_rGZvcPDSoHMpOVm3v4Zimn2y1ubHZB8YNN6lVzOrQhGQ-0_HSkDr214gmPX2DthzrzRVOF6V0kBO");
